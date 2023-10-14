4f69ac43d733b905855788bef30619399947bb38
pross1312 <dangvinhtuong12@gmail.com>
2023-10-14 19:52:46 +0700

    make server serve file

--- server/main.go
+++ server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+    "runtime"
+    "strings"
+    "path/filepath"
     "os"
     "fmt"
     "encoding/json"
@@ -11,6 +14,7 @@ import (
 const (
     SERVER_ADDR = "localhost:9999"
     SERVER_DATA_FILE_PATH = "dictionary_data"
+    WEB_ROOT = "../websrc"
 )
 
 type Entry struct {
@@ -23,8 +27,12 @@ type Dictionary = map[string]Entry
 type MyServer struct {}
 
 var (
-    path_sep string
     Dict = make(Dictionary)
+    content_types = map[string]string{
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "text/javascript",
+    }
 )
 
 func prettyPrint(i interface{}) string {
@@ -37,26 +45,60 @@ func dump_request(req *http.Request) {
     fmt.Println(string(req_str))
 }
 
+func independent_path(file_path string) string { // platform independent
+    if runtime.GOOS == "Windows" {
+        return strings.ReplaceAll(file_path, "/", "\\")
+    }
+    return file_path
+}
+
+func process_query(wt http.ResponseWriter, req *http.Request) {
+    key := req.URL.Query().Get("key")
+    fmt.Printf("[INFO] Client request for key = `%s`\n", key)
+    if entry, found := Dict[key]; found {
+        json_data, err := json.Marshal(entry)
+        if err != nil {
+            fmt.Printf("[ERROR] %s\n", err.Error())
+            wt.WriteHeader(http.StatusInternalServerError)
+            wt.Write([]byte(fmt.Sprintf("[ERROR] %s\n\t[INFO] Can't parse json", err.Error())))
+        } else {
+            wt.Header().Set("Content-Type", "application/json")
+            wt.WriteHeader(http.StatusOK)
+            wt.Write(json_data)
+        }
+    } else {
+        wt.WriteHeader(http.StatusNotFound)
+        wt.Write([]byte(fmt.Sprintf("No entry for %s", key)))
+    }
+}
+
+func serve_file(wt http.ResponseWriter, req *http.Request) {
+    file_path := WEB_ROOT
+    if req.URL.Path == "/" {
+        file_path += "/index.html"
+    } else {
+        file_path += req.URL.Path
+    }
+    fmt.Printf("[INFO] Send file %s\n", independent_path(file_path))
+    data, err := os.ReadFile(independent_path(file_path))
+    if err != nil {
+        wt.WriteHeader(http.StatusNotFound)
+        wt.Write([]byte(fmt.Sprintf("Error: %s\nCan't serve file %s", err.Error(), file_path)))
+    } else {
+        wt.Header().Set("Content-Type", content_types[filepath.Ext(file_path)])
+        wt.Write(data)
+    }
+}
+
 func (sv MyServer) ServeHTTP(wt http.ResponseWriter, req *http.Request) {
-    wt.Header().Set("Access-Control-Allow-Origin", "http://localhost:13123")
+    wt.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
     switch req.Method {
     case "GET":
-        key := req.URL.Query().Get("key")
-        fmt.Printf("[INFO] Client request for key = `%s`\n", key)
-        if entry, found := Dict[key]; found {
-            json_data, err := json.Marshal(entry)
-            if err != nil {
-                fmt.Printf("[ERROR] %s\n", err.Error())
-                wt.WriteHeader(http.StatusInternalServerError)
-                wt.Write([]byte(fmt.Sprintf("[ERROR] %s\n\t[INFO] Can't parse json", err.Error())))
-            } else {
-                wt.Header().Set("Content-Type", "application/json")
-                wt.WriteHeader(http.StatusOK)
-                wt.Write(json_data)
-            }
+        fmt.Printf("[INFO] Client request for %s\n", req.URL.Path)
+        if req.URL.Path == "/query" {
+            process_query(wt, req)
         } else {
-            wt.WriteHeader(http.StatusNotFound)
-            wt.Write([]byte(fmt.Sprintf("No entry for %s", key)))
+            serve_file(wt, req)
         }
     case "POST":
         var entry Entry
@@ -72,7 +114,6 @@ func (sv MyServer) ServeHTTP(wt http.ResponseWriter, req *http.Request) {
         fmt.Fprint(wt, "SUCCESSFULLY")
         save_dict(SERVER_DATA_FILE_PATH);
     case "OPTIONS":
-        wt.Header().Set("Access-Control-Allow-Origin", "http://localhost:13123")
         wt.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST")
         wt.Header().Set("Access-Control-Allow-Headers", "Content-Length, Content-Type")
     default:
@@ -94,10 +135,12 @@ func save_dict(file_path string) {
 }
 
 func main() {
+    fmt.Printf("[INFO] Root on %s\n", WEB_ROOT)
     dict_data, err := os.ReadFile(SERVER_DATA_FILE_PATH)
     if err != nil {
         fmt.Printf("[ERROR] %s\n\t[INFO] Can't read dictionary from file `%s`\n", err.Error(), SERVER_DATA_FILE_PATH)
     }
     json.Unmarshal(dict_data, &Dict)
+    fmt.Printf("[INFO] Server start on %s\n", SERVER_ADDR)
     http.ListenAndServe(SERVER_ADDR, MyServer{})
 }
