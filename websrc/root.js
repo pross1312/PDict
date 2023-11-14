eaf734f7cbf64d4646bf3c0759cb441cbe587dcf
pross1312 <dangvinhtuong12@gmail.com>
2023-11-14 18:05:18 +0700

    handle push/pop history for single page app (enable back button)

--- websrc/root.js
+++ websrc/root.js
@@ -1,11 +1,29 @@
-import {computed} from "vue";
+import {ref, computed} from "vue";
 import navbar from "./nav.js";
 import home from "./home.js";
 export default {
-    provide() {
+    data() {
+        let current_content = ref("home");
+        let current_key = ref("");
+        history.pushState({page: 'home'}, 'PDict', '/');
+        window.onpopstate = function(event) {
+            if (event.state != null) {
+                current_content.value = event.state.content || 'home';
+                current_key.value = event.state.current_key;
+            }
+        }
         return {
-            open_url(url) {
+            current_content,
+            current_key,
+        };
+    },
+provide() {
+        return {
+            change_content: (content) => {
+                history.pushState({content, current_key: this.current_key}, 'PDict', '/');
+                this.current_content = content;
             },
+            set_key: computed(() => (k) => {this.current_key = k;}),
         }
     },
     components: {
